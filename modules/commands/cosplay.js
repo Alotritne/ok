2d1d3f4108fb8828152eb9e93ba7f6306e16c066
Alotritne <87625652+Alotritne@users.noreply.github.com>
2021-10-14 12:43:51 +0700

    Update cosplay.js

--- modules/commands/cosplay.js
+++ modules/commands/cosplay.js
@@ -14,14 +14,14 @@ module.exports.run = async ({ api, event, Currencies }) => {
 	const request = require('request');
 	const fs = require("fs");
 	var money = (await Currencies.getData(event.senderID)).money
-	if (money >= 1000) {
+	if (money >= 0) {
 		axios.get('https://cosplay.hungdz30cm.repl.co/').then(res => {
 		var callback = function () {
 					api.sendMessage({
 						attachment: fs.createReadStream(__dirname + '/cache/cosplay.jpg')
 					}, event.threadID, () => fs.unlinkSync(__dirname + '/cache/cosplay.jpg'), event.messageID);
 				};
-				request(res.data.data).pipe(fs.createWriteStream(__dirname + '/cache/cosplay.jpg')).on("close", callback).then(Currencies.setData(event.senderID, options = {money: money - 1000}));
+				request(res.data.data).pipe(fs.createWriteStream(__dirname + '/cache/cosplay.jpg')).on("close", callback).then(Currencies.setData(event.senderID, options = {money: money - 0}));
 			})
-	} else return api.sendMessage("Bạn cần 1000 đô để xem ảnh ?",event.threadID,event.messageID);
+	} else return api.sendMessage("Bạn cần 0 đô để xem ảnh ?",event.threadID,event.messageID);
 }
